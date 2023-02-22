8dea7328f6c0e83b630b164892a485a843c39a31
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-02-22 13:33:12 +0300

    updated

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -5,12 +5,27 @@ const stripe = new Stripe(`${process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}`);
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
+      //Create Checkout Session
       const params = {
         submit_type: "pay",
         mode: "payment",
         payment_method_types: ["card"],
         billing_address_collection: "auto",
-        shipping_options: [{ shipping_rate: "shr_1Kn3IaEnylLNWUqj5rqhg9oV" }],
+
+        //Coupcon Code
+        allow_promotion_codes: true,
+
+        //Shipping
+        shipping_options: [
+          {
+            shipping_rate: "shr_1MeFOuGehNrOZPF7MVkd3Tju",
+          },
+          {
+            shipping_rate: "shr_1MeFQiGehNrOZPF7uZmz13MB",
+          },
+        ],
+
+        //Add the cart product to the checkout page
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
           const newImage = img
